9c47f515c356e820438effd6fdcb38df0f02f9c9
Dan Ward <danward@gmail.com>
2023-03-28 16:28:45 -0700

    Update param type

--- graphql/queries/get-loot-box-by-token-mint-address-in-hash-list.ts
+++ graphql/queries/get-loot-box-by-token-mint-address-in-hash-list.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_LOOT_BOX_BY_TOKEN_MINT_ADDRESS_IN_HASH_LIST = gql`
-  query GET_LOOT_BOX_BY_TOKEN_MINT_ADDRESS_IN_HASH_LIST($mintAddress: uuid!) {
+  query GET_LOOT_BOX_BY_TOKEN_MINT_ADDRESS_IN_HASH_LIST($mintAddress: jsonb!) {
     sodead_lootBoxes(
       where: {
         costCollections: {
