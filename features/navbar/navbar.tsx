7e73dfe488414176d07d707965105c0853dd0610
warly <warlyware@pm.me>
2023-04-13 21:09:47 -0700

    Fix logo squish

--- features/navbar/navbar.tsx
+++ features/navbar/navbar.tsx
@@ -10,24 +10,22 @@ const Navbar = () => {
   return (
     <div className="relative -mb-16 h-16  px-4 md:px-0 bg-stone-900 shadow-2xl z-100">
       <div className="flex w-full items-center justify-between gap-4 py-2 max-w-6xl mx-auto">
-        <div className="flex space-x-8 items-center font-strange-dreams text-base md:text-xl tracking-wider">
-          <Link href="/" className="flex items-center justify-center">
+        <div className="flex space-x-2 md:space-x-8 items-center font-strange-dreams text-base md:text-xl tracking-wider">
+          <Link href="/" className="flex items-center justify-center flex-none">
             <Image
-              className="h-12 hidden md:block mr-8"
+              className="h-12 hidden md:block mr-2 md:mr-8"
               src="/images/sodead-long-logo.png"
               alt="SoDead Logo"
               height={42}
               width={100}
             />
-            <div className="py-2 -ml-4">
-              <Image
-                className="h-8 w-10 md:hidden"
-                src="/images/sodead-logo.png"
-                alt="SoDead Logo"
-                height={40}
-                width={40}
-              />
-            </div>
+            <Image
+              className="h-8 w-10 md:hidden flex-none flex-shrink-0"
+              src="/images/sodead-logo.png"
+              alt="SoDead Logo"
+              height={40}
+              width={40}
+            />
           </Link>
           <Link href="/hunt/active">Hunts</Link>
           <Link href="/loot-box/active">Lootboxes</Link>
