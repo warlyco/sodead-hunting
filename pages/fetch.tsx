df06ff953a4b0924a3e228d9573fe294cceb7d74
warly <warlyware@pm.me>
2023-04-24 11:59:01 -0700

    Add new fetch page

--- pages/fetch.tsx
+++ pages/fetch.tsx
@@ -1,25 +1,37 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import collectionHashList from "@/features/hashlist/sodead-full-collection.json";
 import { Metaplex } from "@metaplex-foundation/js";
 import { PublicKey } from "@solana/web3.js";
 import { fetchNftsWithMetadata } from "@/utils/nfts/fetch-nfts-with-metadata";
 import { addTraitsToDb } from "@/utils/nfts/add-traits-to-db";
 import axios from "axios";
+import { useAdmin } from "@/hooks/admin";
+import { useRouter } from "next/router";
+import { ContentWrapper } from "@/features/UI/content-wrapper";
+import { FormWrapper } from "@/features/UI/forms/form-wrapper";
+import { FormInputWithLabel } from "@/features/UI/forms/form-input-with-label";
+import { useFormik } from "formik";
+import { useUser } from "@/hooks/user";
+import { SubmitButton } from "@/features/UI/buttons/submit-button";
 
 export default function FetchPage() {
   const { publicKey } = useWallet();
   const { connection } = useConnection();
+  const { isAdmin } = useAdmin();
+  const router = useRouter();
+  const [mintAddress, setMintAddress] = useState("");
+  const { user, setUser } = useUser();
 
   const fetchCollection = useCallback(async () => {
     // get first 10 items from collectionHashList
-    const START = 6000;
-    const END = collectionHashList.length;
+    // const START = 6000;
+    // const END = collectionHashList.length;
     // const selection = collectionHashList.slice(START, END);
-    const selection = ["4d63Sz7MgFoFjhebwpZ2Z175BUJvA3LC4StWRveRhAT8"];
+    const selection = [mintAddress];
+    if (!selection.length) return;
     console.log({
       selection: selection.length,
-
       total: collectionHashList.length,
     });
     if (!publicKey || !connection) return;
@@ -44,7 +56,7 @@ export default function FetchPage() {
 
     await addTraitsToDb(
       nftsWithMetadata,
-      "334a2b4f-b0c6-4128-94b5-0123cb1bff0a"
+      "334a2b4f-b0c6-4128-94b5-0123cb1bff0a" // sodead
     );
     console.log({ nftsWithMetadata });
     console.log(nftMetasFromMetaplex.length, nftsWithMetadata.length);
@@ -54,15 +66,45 @@ export default function FetchPage() {
     });
 
     console.log(nftMetasFromMetaplex, nftsWithMetadata, data);
-  }, [publicKey, connection]);
+  }, [mintAddress, publicKey, connection]);
+
+  const formik = useFormik({
+    initialValues: {
+      mintAddress: "",
+    },
+    onSubmit: async ({ mintAddress }) => {
+      setMintAddress(mintAddress);
+      fetchCollection();
+    },
+  });
 
   useEffect(() => {
-    fetchCollection();
-  }, [connection, fetchCollection, publicKey]);
+    if (!user) return;
+    if (user && !isAdmin) {
+      router.push("/");
+      return;
+    }
+  }, [connection, publicKey, isAdmin, router, user]);
+
+  if (!isAdmin) return null;
 
   return (
-    <div>
-      <h1>Fetch Page</h1>
-    </div>
+    <ContentWrapper>
+      <FormWrapper onSubmit={formik.handleSubmit}>
+        <FormInputWithLabel
+          label="Mint address"
+          name="mintAddress"
+          value={formik.values.mintAddress}
+          onChange={formik.handleChange}
+        />
+      </FormWrapper>
+      <div className="flex w-full justify-center mt-8">
+        <SubmitButton
+          isSubmitting={formik.isSubmitting}
+          onClick={formik.handleSubmit}
+          disabled={!formik.values.mintAddress}
+        />
+      </div>
+    </ContentWrapper>
   );
 }
