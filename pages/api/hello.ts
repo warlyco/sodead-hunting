fc264e6d308e1b4e65c1aee2a64cfb0f3e102b35
warly <warlyware@pm.me>
2023-04-30 00:20:33 -0700

    Add ip test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  ip: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const ip = req.socket.localAddress;
+
+  console.log(`Poking endpoints from ${ip}`);
+  if (!ip) {
+    return res.status(400).json({ ip: "No IP" });
+  }
+  res.status(200).json({ ip });
 }
