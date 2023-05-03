73ab9ddaa05abb23e04976e5a5fb7c6d1c4825f4
warly <warlyware@pm.me>
2023-05-03 11:05:53 -0700

    Fix vertical centering

--- pages/me.tsx
+++ pages/me.tsx
@@ -13,6 +13,8 @@ import { getUserDiscordAccount } from "@/utils/user";
 import dynamic from "next/dynamic";
 import { useLazyQuery } from "@apollo/client";
 import { useUser } from "@/hooks/user";
+import { ContentWrapper } from "@/features/UI/content-wrapper";
+import { ContentWrapperYAxisCenteredContent } from "@/features/UI/content-wrapper-y-axis-centered-content";
 
 export type Wallet = {
   address: string;
@@ -61,61 +63,65 @@ const Home: NextPage = () => {
   return (
     <>
       <SharedHead />
-      <Panel className="flex flex-col items-center px-16">
-        <>
-          {!!user?.imageUrl || !!discordAccount?.imageUrl ? (
-            <ImageWithFallback
-              className="rounded-2xl bg-stone-900 p-2 mb-8 mt-4"
-              src={user?.imageUrl || discordAccount?.imageUrl}
-              alt="SoDead Logo"
-              width={200}
-              height={200}
-            />
-          ) : (
-            <ImageWithFallback
-              className="rounded-2xl bg-stone-900 p-2 mb-8 mt-4"
-              src="/images/sodead-logo.png"
-              alt="SoDead Logo"
-              width={200}
-              height={200}
-            />
-          )}
-
-          {!!user && !user?.accounts?.[0] ? (
+      <ContentWrapper className="w-full justify-center items-center">
+        <ContentWrapperYAxisCenteredContent>
+          <Panel className="flex flex-col items-center px-16">
             <>
-              <div className="flex w-full justify-between text-xl mb-2">
-                <div className="w-1/3">Name:</div>
-                <div className="w-2/3 truncate text-right">{user.name}</div>
-              </div>
-              <div className="flex w-full justify-between text-xl mb-2">
-                <div>Wallet:</div>
-                {getAbbreviatedAddress(user?.primaryWallet?.address)}
-              </div>
-              {!!user.email ||
-                (!!discordAccount?.email && (
-                  <div className="flex w-full justify-between text-xl mb-2">
-                    <div>Email:</div>
-                    <div className="w-2/3 truncate text-right">
-                      {user?.email || discordAccount?.email}
-                    </div>
-                  </div>
-                ))}
-            </>
-          ) : (
-            <>
-              {!!publicKey ? (
-                <div className="text-center py-4">
-                  <LoginWithDiscord user={user || undefined} />
-                </div>
+              {!!user?.imageUrl || !!discordAccount?.imageUrl ? (
+                <ImageWithFallback
+                  className="rounded-2xl bg-stone-900 p-2 mb-8 mt-4"
+                  src={user?.imageUrl || discordAccount?.imageUrl}
+                  alt="SoDead Logo"
+                  width={200}
+                  height={200}
+                />
               ) : (
-                <div className="text-center mb-4">
-                  <WalletButton />
-                </div>
+                <ImageWithFallback
+                  className="rounded-2xl bg-stone-900 p-2 mb-8 mt-4"
+                  src="/images/sodead-logo.png"
+                  alt="SoDead Logo"
+                  width={200}
+                  height={200}
+                />
+              )}
+
+              {!!user && !user?.accounts?.[0] ? (
+                <>
+                  <div className="flex w-full justify-between text-xl mb-2">
+                    <div className="w-1/3">Name:</div>
+                    <div className="w-2/3 truncate text-right">{user.name}</div>
+                  </div>
+                  <div className="flex w-full justify-between text-xl mb-2">
+                    <div>Wallet:</div>
+                    {getAbbreviatedAddress(user?.primaryWallet?.address)}
+                  </div>
+                  {!!user.email ||
+                    (!!discordAccount?.email && (
+                      <div className="flex w-full justify-between text-xl mb-2">
+                        <div>Email:</div>
+                        <div className="w-2/3 truncate text-right">
+                          {user?.email || discordAccount?.email}
+                        </div>
+                      </div>
+                    ))}
+                </>
+              ) : (
+                <>
+                  {!!publicKey ? (
+                    <div className="text-center py-4">
+                      <LoginWithDiscord user={user || undefined} />
+                    </div>
+                  ) : (
+                    <div className="text-center mb-4">
+                      <WalletButton />
+                    </div>
+                  )}
+                </>
               )}
             </>
-          )}
-        </>
-      </Panel>
+          </Panel>
+        </ContentWrapperYAxisCenteredContent>
+      </ContentWrapper>
     </>
   );
 };
